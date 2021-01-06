c8c16eab9c773fb8a2955845e5f2ef54af2e5026
Adam Drewery <adrewery87@gmail.com>
2021-01-06 08:51:50 -0600

    Initial commit.

---
+++ lib/quadrature.ex
@@ -0,0 +1,18 @@
+defmodule Quadrature do
+  @moduledoc """
+  Documentation for Quadrature.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Quadrature.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/quadrature_test.exs
@@ -0,0 +1,8 @@
+defmodule QuadratureTest do
+  use ExUnit.Case
+  doctest Quadrature
+
+  test "greets the world" do
+    assert Quadrature.hello() == :world
+  end
+end
